59d1cef6cadf0b64f2dd53e666dae4c3679ffa91
Kevin Rangel <code@venikx.com>
2022-10-23 20:11:09 +0300

    chore: add typescript typings in theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,9 +3,12 @@ import radii from './radius';
 import space from './space';
 import { typography } from './typography';
 
-export default {
+const Theme = {
   radii,
   colors,
   ...typography,
   space,
-};
+} as const;
+
+export type ThemeType = typeof Theme;
+export default Theme;
