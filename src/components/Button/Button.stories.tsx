184ee3a7aa5d110c6fc556657258448bd9485bd4
Kevin Rangel <code@venikx.com>
2022-10-23 22:58:49 +0300

    feat: setup Button stories based on Figma design

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import styled from 'styled-components';
+import { Button, IButtonProps } from './Button';
+
+export default {
+  title: 'Inputs/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = ({
+  children,
+  ...args
+}: IButtonProps) => <Button {...args}>{children}</Button>;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Primary Button',
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px 96px;
+`;
+
+const AllTemplate: ComponentStory<typeof Button> = () => (
+  <Container>
+    <Button>Primary Button</Button>
+    <Button variant="outline">Secondary Button</Button>
+    <Button variant="ghost">Tertiary Button</Button>
+    <Button isDisabled>Primary Button</Button>
+    <Button isDisabled variant="outline">
+      Secondary Button
+    </Button>
+    <Button isDisabled variant="ghost">
+      Tertiary Button
+    </Button>
+    <Button>
+      Very Long Button With a Loads of Content That is Really Long Actually
+    </Button>
+    <Button>
+      Another Very Long Button With a Loads of Content That is Really Long
+      Actually
+    </Button>
+  </Container>
+);
+
+export const AllButtons = AllTemplate.bind({});
